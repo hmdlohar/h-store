f6fe632b35af9e2f2a2a61a56de4e3ad5d9130c8
hmdlohar <hmdlohar@gmail.com>
2025-05-10 16:40:05 +0530

    Add PostHog integration and update environment configuration
    - Added PostHog client setup in a new posthog.js file for analytics tracking.
    - Updated env.example to include PostHog key and host.
    - Configured Next.js rewrites for PostHog API endpoints in next.config.mjs.
    - Integrated PostHogProvider in the main application layout to enable analytics tracking on page views.
    - Updated package.json and package-lock.json to include PostHog dependencies.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,6 +3,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 
+import { useEffect } from "react";
+import { Router } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../theme";
@@ -11,13 +16,32 @@ import { queryClient } from "@/common/ReactQueryClient";
 import TawkChatWidget from '../common/TawkChatWidget';
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+      debug: process.env.NODE_ENV === "development",
+    });
+
+    const handleRouteChange = () => posthog.capture("$pageview");
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-        <TawkChatWidget />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+          <TawkChatWidget />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
